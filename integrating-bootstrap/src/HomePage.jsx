6e369a1daad1300f76c3a901687a00ab920f4145
Clinton.G <clintongoin@gmail.com>
2024-09-16 18:57:37 -0500

    integrating-bootstrap

---
+++ integrating-bootstrap/src/HomePage.jsx
@@ -0,0 +1,14 @@
+import { Button, Container } from 'react-bootstrap';
+import './HomePage.css';
+import './Bowser-Peaches.jpg'
+
+
+const HomePage = () => {
+  return (
+    <Container className="shop-now-button">
+      <Button className='shop-now-text'>Shop Now</Button>
+    </Container>
+  );
+};
+
+export default HomePage;
